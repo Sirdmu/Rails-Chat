12caace15218e4cf2a510bd85d12ac0957adafaa
Sirdmu <din.siraj7@gmail.com>
2019-12-01 22:36:48 +0000

    Getting the giphy api to work, now need to render the html to the actual source image

--- app/controllers/room_messages_controller.rb
+++ app/controllers/room_messages_controller.rb
@@ -2,11 +2,21 @@ class RoomMessagesController < ApplicationController
   before_action :load_entities
 
   def create
-    response = HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=#{ENV['GIPHY_API_KEY']}&q=cheeseburger&limit=1")
-    p response.parsed_response["data"][0]["embed_url"]
+    message = params.dig(:room_message, :message)
+    has_gif = message.split.include?"!gif"
+    if has_gif 
+      query_index = message.split.index("!gif") + 1
+      term = message.split[query_index]
+      response = HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=#{ENV['GIPHY_API_KEY']}&q=#{term}&limit=1")
+      gif_url = response.parsed_response["data"][0]["embed_url"]
+      message = "<image src='#{gif_url}'>"
+    end
+    # p message
+    # response = HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=#{ENV['GIPHY_API_KEY']}&q=cheeseburger&limit=1")
+    # p response.parsed_response["data"][0]["embed_url"]
     @room_message = RoomMessage.create user: current_user,
                                        room: @room,
-                                       message: params.dig(:room_message, :message)
+                                       message: message
     RoomChannel.broadcast_to @room, @room_message
   end
 
