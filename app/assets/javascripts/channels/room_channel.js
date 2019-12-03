69cdd0096aa8446275480983185df4a3e2800e77
Sirdmu <din.siraj7@gmail.com>
2019-12-03 03:19:53 +0000

    Trying to push to heroku part 2

--- app/assets/javascripts/channels/room_channel.js
+++ app/assets/javascripts/channels/room_channel.js
@@ -16,7 +16,7 @@ $(function() {
           // if (data.link) {
           // } else {
             var content = messageTemplate.children().clone(true, true);
-            var img = $('<div style="width:100%;height:0;padding-bottom:100%;position:relative;"><iframe src="${data.link}" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div>)
+            var img = $(`<iframe src="${data.link}" ></iframe>`);
             console.log(data.link)
             content.find('[data-role="user-avatar"]').attr('src', data.user_avatar_url);
             content.find('[data-role="message-text"]').text(data.message);
